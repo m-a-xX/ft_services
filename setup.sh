619635ee983e4a98b035ba5415a8e5e3a56efadd
mavileo <mavileo@student.42.fr>
2020-08-06 07:31:02 +0200

    setup.sh install minikube, metallb and deploy nginx

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# install a new minikube
+minikube delete
+minikube start
+eval $(minikube docker-env)
+
+# install metallb and creaate deployment and speaker deamon
+sh srcs/metallb/install.sh
+kubectl create -f srcs/metallb/metallb.yaml
+
+# deploy nginx cluster
+docker build -t nginx srcs/nginx
+kubectl apply -f srcs/nginx/nginx.yaml
+
+
