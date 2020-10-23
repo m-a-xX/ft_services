c3ec1ed31b557ad10ed30083d137a6212c7c854b
mavileo <mavileo@student.42.fr>
2020-10-23 19:36:57 +0200

    pma cookie auth not http

--- srcs/mysql/files/start.sh
+++ srcs/mysql/files/start.sh
@@ -8,8 +8,8 @@ service mariadb restart &> /dev/null
 
 mysql --user=root << EOF
   CREATE DATABASE wordpress;
-  CREATE USER 'wp_user'@'%' IDENTIFIED BY '$WP_ADMIN_PASS';
-  GRANT ALL ON wordpress.* TO 'wp_user'@'%' IDENTIFIED BY '$WP_USER_PASS' WITH GRANT OPTION;
+  CREATE USER 'wp_user'@'%' IDENTIFIED BY 'wp_pass';
+  GRANT ALL ON wordpress.* TO 'wp_user'@'%' IDENTIFIED BY 'wp_pass' WITH GRANT OPTION;
   CREATE USER 'admin'@'%' IDENTIFIED BY '$ADMIN_PASS';
   GRANT ALL ON *.* TO 'admin'@'%' IDENTIFIED BY '$ADMIN_PASS' WITH GRANT OPTION;
   FLUSH PRIVILEGES;
