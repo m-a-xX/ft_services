29f13b28c5972c9be890f17492b8d7d38b375fe0
mavileo <mavileo@student.42.fr>
2020-11-20 06:19:26 +0100

    mysql persistence and pma wp liveness probe

--- srcs/mysql/files/start.sh
+++ srcs/mysql/files/start.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+if [ ! -d /var/lib/mysql/wordpress ]
+then
 nohup ./init.sh > /dev/null 2>&1 &
+fi
 
 sed -i 's/skip-networking/#skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
 telegraf &
+if [ ! -d /var/lib/mysql/mysql ]
+then
 /usr/bin/mysql_install_db --user=mysql --datadir="/var/lib/mysql"
+fi
 /usr/bin/mysqld_safe --datadir="/var/lib/mysql"
